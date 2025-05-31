0d5a74dcf8f5cf940bfb2cd3e2787edb702af0f7
Anders Revsgaard <anders@itfoxtec.com>
2025-05-31 11:56:43 +0200

    cert kustomize.yaml

--- cluster/ovhcloud/test/modules/cert-manager/main.tf
+++ cluster/ovhcloud/test/modules/cert-manager/main.tf
@@ -19,11 +19,29 @@ resource "kubernetes_secret" "cloudflare-api-secret" {
   depends_on = [ kubernetes_namespace.cert-manager-ns ]
 }
 
-resource "kubectl_manifest" "letsencrypt-kustomize" {
-  yaml_body = templatefile("${path.module}/letsencrypt-kustomize.yaml", {  
-      letsencrypt_email = var.letsencrypt_email,
-      cloudflare_email  = var.cloudflare_email
-    })
-
-  depends_on = [ kubernetes_namespace.cert-manager-ns ]
+data "kubectl_kustomize_documents" "letsencrypt-manifests" {
+    target = "https://github.com/ITfoxtec/FoxIDs.Kubernetes-CD/cluster/ovhcloud/test/modules/cert-manager/"
+#    target = templatefile("${path.module}/letsencrypt-kustomize.yaml", {  
+#      letsencrypt_email = var.letsencrypt_email,
+#      cloudflare_email  = var.cloudflare_email
+#    })
 }
+
+resource "kubectl_manifest" "letsencrypt-kustomize" {
+    count     = length(data.kubectl_kustomize_documents.letsencrypt-manifests.documents)
+    yaml_body = templatefile(element(data.kubectl_kustomize_documents.letsencrypt-manifests.documents, count.index), {  
+        letsencrypt_email = var.letsencrypt_email,
+        cloudflare_email  = var.cloudflare_email
+      })
+
+    depends_on = [ kubernetes_namespace.cert-manager-ns ]
+}
+
+#resource "kubectl_manifest" "letsencrypt-kustomize" {
+#  yaml_body = templatefile("${path.module}/letsencrypt-kustomize.yaml", {  
+#      letsencrypt_email = var.letsencrypt_email,
+#      cloudflare_email  = var.cloudflare_email
+#    })
+#
+#  depends_on = [ kubernetes_namespace.cert-manager-ns ]
+#}
