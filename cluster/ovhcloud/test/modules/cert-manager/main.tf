9dd605ff080cd799fde661b7f2bbb58991b9a659
Anders Revsgaard <anders@itfoxtec.com>
2025-05-31 11:33:34 +0200

    app-app

--- cluster/ovhcloud/test/modules/cert-manager/main.tf
+++ cluster/ovhcloud/test/modules/cert-manager/main.tf
@@ -1,6 +1,13 @@
+resource "kubernetes_namespace" "cert-manager-ns" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
 resource "kubernetes_secret" "cloudflare-api-secret" {
   metadata {
     name = "cloudflare-api-token"
+    namespace = "cert-manager"
   }
 
   data = {
@@ -8,6 +15,8 @@ resource "kubernetes_secret" "cloudflare-api-secret" {
   }
 
   type = "Opaque"
+
+  depends_on = [ kubernetes_namespace.cert-manager-ns ]
 }
 
 resource "kubectl_manifest" "letsencrypt-kustomize" {
@@ -15,4 +24,6 @@ resource "kubectl_manifest" "letsencrypt-kustomize" {
       letsencrypt_email = var.letsencrypt_email,
       cloudflare_email  = var.cloudflare_email
     })
+
+  depends_on = [ kubernetes_namespace.cert-manager-ns ]
 }
