c59d7c3804ea1c7f4728021b07014c0aeed58a8c
Anders Revsgaard <anders@itfoxtec.com>
2025-08-24 09:00:30 +0200

    helm updates

--- multi_nodes/terraform_setup/modules/argocd/main.tf
+++ multi_nodes/terraform_setup/modules/argocd/main.tf
@@ -1,9 +1,9 @@
+# https://artifacthub.io/packages/helm/argo/argo-cd
 # https://argo-cd.readthedocs.io/en/stable/getting_started/
 # https://argoproj.github.io/cd/
 # https://argo-cd.readthedocs.io/
 # https://argoproj.github.io/argo-helm/
 # https://github.com/argoproj/argo-helm
-# https://artifacthub.io/packages/helm/argo/argo-cd
 # https://medium.com/transmit-security-engineering/building-argocd-ecosystem-with-secret-management-the-gitops-way-part-i-49b4921f7c1f
 
 resource "helm_release" "argocd" {
@@ -13,7 +13,7 @@ resource "helm_release" "argocd" {
   dependency_update   = true
   repository          = "https://argoproj.github.io/argo-helm"  
   chart               = "argo-cd"
-  version             = "8.0.12"
+  version             = "8.3.0"
   cleanup_on_fail     = true
 
   # Argo admin password
